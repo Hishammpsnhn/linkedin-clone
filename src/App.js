e62d6ead4c9236877c62d8ba87df52be2110a6dc
hisham <hishammpsn@gmail.com>
2023-07-21 20:20:55 +0530

    add images and nav folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import Header from './components/navbar/Header';
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+   <>
+   <Header/>
+   </>
   );
 }
 
---
+++ src/components/navbar/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from 'styled-components'
+
+const Container = styles.div`
+    background-color: white
+    
+`
+const Header = () => {
+  return <Container>Header</Container>;
+};
+
+export default Header;
